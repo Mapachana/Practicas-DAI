665069cad4f0cb3c2482d81fae7898372c3776de
Mapachana <anabuenrua99@gmail.com>
2021-10-29 09:18:59 +0200

    Añado enlace para login

--- p2/app/app.py
+++ p2/app/app.py
@@ -1,5 +1,5 @@
 #./app/app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
@@ -7,6 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'prueba':
+            error = 'Invalid credentials'
+        else:
+            return redirect('/')
+    return render_template('login.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
