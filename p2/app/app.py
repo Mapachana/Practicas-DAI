66e7ad28ac692a40e1aa0f08084c6f4fef0891de
Mapachana <anabuenrua99@gmail.com>
2021-10-31 01:00:19 +0200

    Creo sesion

--- p2/app/app.py
+++ p2/app/app.py
@@ -1,25 +1,48 @@
 #./app/app.py
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 import modelo 
 
 app = Flask(__name__)
+app.secret_key = 'esto-es-una-clave-muy-secreta'
+
           
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    if 'username' in session:
+        nombre = session['username']
+    else:
+        nombre = ''
+    return render_template('index.html', username=nombre)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+    nombre = ""
+    print("PRINT FUNCIONA")
     
     if request.method == 'POST':
         if modelo.comprobar_login(request.form['username'], request.form['password']):
-            return redirect('/')
+            aux = request.form['username']
+            session['username'] = aux
+            print("entro aqui y vale")
+            print(session['username'])
+            return redirect('/login')
         else:
             error = "Los datos introducidos no son validos"
-    
-    return render_template('login.html')
+    if 'username' in session:
+        nombre = session['username']
+        print(nombre)
+    else:
+        nombre = ''
+    print(nombre)
+
+    return render_template('login.html', username=nombre)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 @app.errorhandler(404)
 def page_not_found(e):
