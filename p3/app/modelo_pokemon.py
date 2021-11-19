91fd3a7486329b1226edf90531a0957f02eeccc5
Mapachana <anabuenrua99@gmail.com>
2021-11-19 10:53:19 +0100

    Añado busqueda con regex

--- p3/app/modelo_pokemon.py
+++ p3/app/modelo_pokemon.py
@@ -2,6 +2,7 @@ from pickleshare import *
 
 from pymongo import MongoClient
 
+
 class DBPokemon:
     def __init__(self):
         client = MongoClient("mongo", 27017) # Conectar al servicio (docker) "mongo" en su puerto estandar
@@ -9,7 +10,10 @@ class DBPokemon:
         self.coll = self.db.samples_pokemon
     
     def buscar(self, clave=None):
-        return self.coll.find({'name': clave})
+        clave = '.*'+clave+'.*'
+
+
+        return self.coll.find({"$or": [{'name': { "$regex": clave}}, {'type': { "$regex": clave}}]})
 
     def add_pokemon(self, nombre, img, type, height, weight, candy, egg):
         pass
